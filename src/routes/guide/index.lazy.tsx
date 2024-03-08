6c5402c4cf0180199d70f6c2e33fac228c393c41
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-08 19:06:22 +0300

    Sections mapping from config

--- src/routes/guide/index.lazy.tsx
+++ src/routes/guide/index.lazy.tsx
@@ -1,6 +1,6 @@
 import {createLazyFileRoute, Link} from "@tanstack/react-router";
 import Section from "../../components/Section.tsx";
-import {intro, kits, keycaps, switches, stabilizers} from '../../config/section.json'
+import sections from '../../config/section.json';
 import {Button} from "@mantine/core";
 
 const Guide = () => {
@@ -8,36 +8,13 @@ const Guide = () => {
         <main className="main">
             <div className="main__body">
                 <Button component={Link} to="/">Вернуться</Button>
-                <Section title={intro.title}
-                         description={intro.description}
-                         imageURL={intro.imageURL}
-                         imageAlt={intro.imageAlt}
-                         pageURL={intro.pageURL}
-                />
-                <Section title={kits.title}
-                         description={kits.description}
-                         imageURL={kits.imageURL}
-                         imageAlt={kits.imageAlt}
-                         pageURL={kits.pageURL}
-                />
-                <Section title={keycaps.title}
-                         description={keycaps.description}
-                         imageURL={keycaps.imageURL}
-                         imageAlt={keycaps.imageAlt}
-                         pageURL={keycaps.pageURL}
-                />
-                <Section title={switches.title}
-                         description={switches.description}
-                         imageURL={switches.imageURL}
-                         imageAlt={switches.imageAlt}
-                         pageURL={switches.pageURL}
-                />
-                <Section title={stabilizers.title}
-                         description={stabilizers.description}
-                         imageURL={stabilizers.imageURL}
-                         imageAlt={stabilizers.imageAlt}
-                         pageURL={stabilizers.pageURL}
-                />
+                {
+                    sections.map(({ title, description, imageURL, imageAlt, pageURL }) => {
+                        return (
+                            <Section title={title} description={description} imageURL={imageURL} imageAlt={imageAlt} pageURL={pageURL} />
+                        )
+                    })
+                }
             </div>
         </main>
     )
