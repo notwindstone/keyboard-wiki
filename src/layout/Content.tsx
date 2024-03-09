9ad0e74c9a76d9102db19efdbced43a010145ec3
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-09 21:33:29 +0300

    zero-md styles reset

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -3,7 +3,11 @@ const Content = () => {
         <article className="main__article">
                 {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
                 {/* @ts-ignore */}
-            <zero-md src="https://raw.githubusercontent.com/windstone-aristotle-yellow/keyboard-wiki/main/README.md"></zero-md>
+            <zero-md src="https://raw.githubusercontent.com/windstone-aristotle-yellow/keyboard-wiki/main/README.md">
+                    <template>
+                            <style></style>
+                    </template>
+            </zero-md>
         </article>
     )
 }
