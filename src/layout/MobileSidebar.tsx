a62bdb9e0b64f5ab6d81cb5adef49451d0b609f6
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-10 18:34:20 +0300

    Added button to mobile sidebar

--- src/layout/MobileSidebar.tsx
+++ src/layout/MobileSidebar.tsx
@@ -1,5 +1,8 @@
 import SideNavItem from "../components/SideNavItem.tsx";
 import sidebar from "../config/sidebar.json";
+import {Button} from "@mantine/core";
+import {IconCompass} from "@tabler/icons-react";
+import {Link} from "@tanstack/react-router";
 
 const MobileSidebar = (props: { hidden: boolean }) => {
     const { hidden } = props
@@ -7,6 +10,8 @@ const MobileSidebar = (props: { hidden: boolean }) => {
     return (
         <aside className="mobile-sidebar" hidden={hidden}>
             <div className="mobile-sidebar__body">
+                <Button leftSection={<IconCompass stroke={1.5}/>} justify="flex-start" variant="filled" component={Link}
+                        to="/guide/" className="sidebar__item">Вернуться</Button>
                 <SideNavItem title="Введение" sections={sidebar.intro}/>
                 <SideNavItem title="Базы" sections={sidebar.kits}/>
                 <SideNavItem title="Кейкапы" sections={sidebar.keycaps}/>
