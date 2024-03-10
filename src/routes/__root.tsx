21ae55113b3c39b10ca1c6e37f36e3d884a200f4
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-10 15:07:20 +0300

    Disable Tanstack Router Devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,4 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import Header from "../layout/Header.tsx";
 
 export const Route = createRootRoute({
@@ -7,7 +6,6 @@ export const Route = createRootRoute({
         <>
             <Header />
             <Outlet />
-            <TanStackRouterDevtools />
         </>
     )
 })
