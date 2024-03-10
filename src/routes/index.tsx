08d9b898107d9e161548891dc131c84790b2f0bb
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-10 11:23:37 +0300

    Test markdown class

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,20 +7,23 @@ const Index = () => {
     return (
         <main className="main">
             <div className="main__body">
-                {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-                {/* @ts-ignore */}
-                <zero-md src={mainPageURL}>
-                    <template>
-                        <style></style>
-                    </template>
+                <div className="main__markdown-body">
                     {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
                     {/* @ts-ignore */}
-                </zero-md>
-                <img
-                    className="main__poster"
-                    src="https://dangkeebs.com/cdn/shop/files/2_2645fa39-eab3-4170-9364-34ca58e7e671.jpg?v=1688070357&width=1445"
-                    alt="Meletrix Zoom75"
-                />
+                    <zero-md src={mainPageURL}>
+                        <template>
+                            <style>
+                            </style>
+                        </template>
+                        {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                        {/* @ts-ignore */}
+                    </zero-md>
+                    <img
+                        className="main__poster"
+                        src="https://dangkeebs.com/cdn/shop/files/2_2645fa39-eab3-4170-9364-34ca58e7e671.jpg?v=1688070357&width=1445"
+                        alt="Meletrix Zoom75"
+                    />
+                </div>
             </div>
         </main>
     )
