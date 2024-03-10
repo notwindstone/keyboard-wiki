a1657d04f68ada2572717d357ca8570b0ddec59a
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-10 10:36:17 +0300

    index route initial code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,16 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+const Index = () => {
+    return (
+        <main className="main">
+            <div className="main__body">
+                <p>asdf</p>
+                <p>asdf</p>
+            </div>
+        </main>
+    )
+}
+
 export const Route = createFileRoute('/')({
-  component: () => <div>Hello /!</div>
+  component: Index
 })
