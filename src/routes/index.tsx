b8c4d3cebe5577bcdcdd87375d443080e43ac215
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-11 20:33:11 +0300

    Change lazy routes to non-lazy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createLazyFileRoute('/')({
+export const Route = createFileRoute('/')({
     component: Index,
 })
 
