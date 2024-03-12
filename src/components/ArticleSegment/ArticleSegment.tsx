6b27323ce231aa11834588b955d8fada5f4a6335
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-12 15:05:16 +0300

    Cards import data from config

--- src/components/ArticleSegment/ArticleSegment.tsx
+++ src/components/ArticleSegment/ArticleSegment.tsx
@@ -2,49 +2,19 @@ import classes from './ArticleSegment.module.css'
 import {AspectRatio, Card, Container, Image, SimpleGrid, Text, ThemeIcon, Title} from "@mantine/core";
 import {IconExternalLink} from "@tabler/icons-react";
 import {Link} from "@tanstack/react-router";
+import articles from '../../configs/articles.json'
 
 const ArticleSegment = () => {
-    const mockdata = [
-        {
-            title: 'Основы',
-            image:
-                'https://d2dqts71r73516.cloudfront.net/1658189887894_resized-red-1.jpeg',
-            date: 'Введение, термины, размеры, раскладки',
-            link: '/articles/intro'
-        },
-        {
-            title: 'Базы для клавиатур',
-            image:
-                'https://thekey.company/cdn/shop/products/M1Website-4_1024x1024.jpg?v=1681395260',
-            date: 'Плейты, маунты',
-            link: '/articles/kits'
-        },
-        {
-            title: 'Переключатели',
-            image:
-                'https://cdn.shopify.com/s/files/1/1679/2319/files/DSC08448_1024x1024.jpg?v=1597791918',
-            date: 'Виды, анатомия, график силы нажатия',
-            link: '/articles/switches'
-        },
-        {
-            title: 'Кейкапы',
-            image:
-                'https://pkkeyboards.com/cdn/shop/products/epbtblackwhiteabsdoubleshotthickness.jpg?v=1651026662&width=1123',
-            date: 'Материалы, виды',
-            link: '/articles/keycaps'
-        },
-    ];
-
-    const cards = mockdata.map((article) => (
+    const cards = articles.map((article) => (
         <Card key={article.title} p="xl" radius="md" component={Link} to={article.link} className={classes.card}>
-            <ThemeIcon variant="default" className={classes.icon}>
+            <ThemeIcon size="xl" variant="default" className={classes.icon}>
                 <IconExternalLink />
             </ThemeIcon>
             <AspectRatio ratio={1920 / 1080}>
-                <Image className={classes.image} src={article.image} />
+                <Image className={classes.image} src={article.imageURL} alt={article.imageAlt} />
             </AspectRatio>
             <Text c="dimmed" size="sm" tt="uppercase" fw={700} mt="md">
-                {article.date}
+                {article.content}
             </Text>
             <Title order={2} className={classes.title} mt={5}>
                 {article.title}
