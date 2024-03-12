93d7dac1c7e0fd3a8f7a434d770f4039ea486614
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-12 15:13:37 +0300

    Card component

--- src/components/ArticleSegment/ArticleSegment.tsx
+++ src/components/ArticleSegment/ArticleSegment.tsx
@@ -1,32 +1,20 @@
 import classes from './ArticleSegment.module.css'
-import {AspectRatio, Card, Container, Image, SimpleGrid, Text, ThemeIcon, Title} from "@mantine/core";
-import {IconExternalLink} from "@tabler/icons-react";
-import {Link} from "@tanstack/react-router";
+import {Container, SimpleGrid} from "@mantine/core";
 import articles from '../../configs/articles.json'
+import ContentCard from "../ContentCard/ContentCard.tsx";
 
 const ArticleSegment = () => {
     const cards = articles.map((article) => (
-        <Card key={article.title} p="xl" radius="md" component={Link} to={article.link} className={classes.card}>
-            <ThemeIcon size="xl" variant="default" className={classes.icon}>
-                <IconExternalLink />
-            </ThemeIcon>
-            <AspectRatio ratio={1920 / 1080}>
-                <Image className={classes.image} src={article.imageURL} alt={article.imageAlt} />
-            </AspectRatio>
-            <Text c="dimmed" size="sm" tt="uppercase" fw={700} mt="md">
-                {article.content}
-            </Text>
-            <Title order={2} className={classes.title} mt={5}>
-                {article.title}
-            </Title>
-        </Card>
+        <ContentCard card={article} />
     ));
 
 
     return (
         <div className={classes.wrapper}>
             <Container size={1400} className={classes.inner}>
-                <SimpleGrid cols={{ base: 1, sm: 2 }}>{cards}</SimpleGrid>
+                <SimpleGrid cols={{ base: 1, sm: 2 }}>
+                    {cards}
+                </SimpleGrid>
             </Container>
         </div>
     )
---
+++ src/components/ContentCard/ContentCard.tsx
@@ -0,0 +1,27 @@
+import {AspectRatio, Card, Image, Text, ThemeIcon, Title} from "@mantine/core";
+import {IconExternalLink} from "@tabler/icons-react";
+import classes from './ContentCard.module.css';
+import {Link} from "@tanstack/react-router";
+
+const ContentCard = (props: { card: any }) => {
+    const { card } = props
+
+    return (
+        <Card key={card.title} p="xl" radius="md" component={Link} to={card.link} className={classes.card}>
+            <ThemeIcon size="xl" variant="default" className={classes.icon}>
+                <IconExternalLink />
+            </ThemeIcon>
+            <AspectRatio ratio={1920 / 1080}>
+                <Image className={classes.image} src={card.imageURL} alt={card.imageAlt} />
+            </AspectRatio>
+            <Text c="dimmed" size="sm" tt="uppercase" fw={700} mt="md">
+                {card.content}
+            </Text>
+            <Title order={2} className={classes.title} mt={5}>
+                {card.title}
+            </Title>
+        </Card>
+    )
+}
+
+export default ContentCard
