5e2be8771e375789b0fa8ee9013c45e50c955a75
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-12 14:09:14 +0300

    HeroBullets are done. Init ArticleSegment files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 import {MobileNavbar} from "../components/MobileNavbar/MobileNavbar.tsx";
 import {HeroBullets} from "../components/HeroBullets/HeroBullets.tsx";
+import {SegmentedControl} from "@mantine/core";
 
 const Index = () => {
     return (
         <MobileNavbar>
             <HeroBullets />
+            <SegmentedControl withItemsBorders={false} data={['React', 'Angular', 'Vue']} />
         </MobileNavbar>
     )
 }
