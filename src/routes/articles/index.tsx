add16a4f91c8b93b0266f6097d6e763f47e20742
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-13 21:58:31 +0300

    ArticleSegment changes

--- src/routes/articles/index.tsx
+++ src/routes/articles/index.tsx
@@ -4,8 +4,11 @@ import ArticleSegment from "../../components/ArticleSegment/ArticleSegment.tsx";
 import {SegmentedControl, Title} from "@mantine/core";
 import articles from '../../configs/articles.json'
 import classes from './index.module.css'
+import {useState} from "react";
 
 const Articles = () => {
+    const [value, setValue] = useState('react')
+
     const subArticles = articles.map((article) => {
         return (
             <ArticleSegment articles={article.content} />
@@ -17,7 +20,6 @@ const Articles = () => {
             article.title
         )
     })
-
     return (
         <MobileNavbar>
             <Title className={classes.title}>Сортировка статей</Title>
@@ -26,7 +28,12 @@ const Articles = () => {
                 control: classes.control,
                 label: classes.label,
                 indicator: classes.indicator
-            }} withItemsBorders={false} color="light-dark(var(--mantine-color-violet-2), var(--mantine-color-violet-9))" data={articleSections} />
+            }}
+                              withItemsBorders={false}
+                              color="light-dark(var(--mantine-color-violet-2), var(--mantine-color-violet-9))"
+                              value={value}
+                              onChange={setValue}
+                              data={articleSections} />
             {subArticles}
         </MobileNavbar>
     )
