68d29eb32c10e00c935581c1a7c4328495ca00c9
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-14 20:17:11 +0300

    Navigation Buttons

--- src/components/ArticleContent/ArticleContent.tsx
+++ src/components/ArticleContent/ArticleContent.tsx
@@ -1,12 +1,52 @@
-import {Container, Title} from "@mantine/core";
+import {Button, Container, Flex, Title} from "@mantine/core";
 import {Link} from "@tanstack/react-router";
+import {IconArrowLeft, IconArrowRight} from "@tabler/icons-react";
+import classes from './ArticleContent.module.css'
 
 const ArticleContent = ({ content }: any) => {
+    const linkToNextPage = "/"
+    const linkToPreviousPage = "/"
+    const nextPage = "Франкенсвитчи"
+    const previousPage = "Виды"
+
     return (
         <div>
             <Container size={1400}>
                 <Link>Вернуться</Link>
                 <Title></Title>
+                <Flex
+                    className={classes.wrapper}
+                    justify='flex-start'
+                    gap='2rem'
+                    wrap='wrap'
+                >
+                    <Button
+                        component={Link}
+                        justify="space-between"
+                        to={linkToPreviousPage}
+                        variant="default"
+                        className={classes.button}
+                        leftSection={<IconArrowLeft stroke={1.2} size={24} />}
+                    >
+                        <div className={classes.buttonContent}>
+                            <p className={classes.buttonTitlePrevious}>Предыдущая страница</p>
+                            <p className={classes.buttonDescriptionPrevious}>{previousPage}</p>
+                        </div>
+                    </Button>
+                    <Button
+                        component={Link}
+                        justify="space-between"
+                        to={linkToNextPage}
+                        variant="default"
+                        className={classes.button}
+                        rightSection={<IconArrowRight stroke={1.2} size={24} />}
+                    >
+                        <div className={classes.buttonContent}>
+                            <p className={classes.buttonTitleNext}>Следующая страница</p>
+                            <p className={classes.buttonDescriptionNext}>{nextPage}</p>
+                        </div>
+                    </Button>
+                </Flex>
             </Container>
         </div>
     )
--- src/routes/articles/switches/forceCurves.tsx
+++ src/routes/articles/switches/forceCurves.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute } from '@tanstack/react-router'
 import MobileNavbar from "../../../components/MobileNavbar/MobileNavbar.tsx";
+import ArticleContent from "../../../components/ArticleContent/ArticleContent.tsx";
 
 const SwitchForceCurves = () => {
     return (
         <MobileNavbar>
-            <></>
+            <ArticleContent content="1234" />
         </MobileNavbar>
     )
 }
