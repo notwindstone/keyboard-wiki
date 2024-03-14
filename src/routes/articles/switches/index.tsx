588a839d04ff854c0e09607b418787e926555a2b
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-14 19:02:36 +0300

    Switches index page

--- src/routes/articles/switches/index.tsx
+++ src/routes/articles/switches/index.tsx
@@ -1,5 +1,22 @@
 import { createFileRoute } from '@tanstack/react-router'
+import ArticleSegment from "../../../components/ArticleSegment/ArticleSegment.tsx";
+import articles from '../../../configs/articles.json'
+import MobileNavbar from "../../../components/MobileNavbar/MobileNavbar.tsx";
+import classes from "./index.module.css";
+import {Title} from "@mantine/core";
+
+const SwitchesIndex = () => {
+    const article = articles[2]
+
+    return (
+        <MobileNavbar>
+            <Title className={classes.title}>Свитчи</Title>
+            <ArticleSegment style={{marginBottom: '1rem'}} key={article.title} articles={article.content} />
+        </MobileNavbar>
+    )
+}
+
 
 export const Route = createFileRoute('/articles/switches/')({
-  component: () => <div>Hello /articles/switches/!</div>
+    component: SwitchesIndex
 })
