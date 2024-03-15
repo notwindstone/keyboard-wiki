45b23ab82c947d0444c1b2a101016429706e53f5
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-15 14:23:09 +0300

    Yor Forgor (what the heck i was doing)

--- src/components/ArticleContent/ArticleContent.tsx
+++ src/components/ArticleContent/ArticleContent.tsx
@@ -1,23 +1,35 @@
-import {Container, Group, Text, Title} from "@mantine/core";
+import {Box, Container, Group, Image, Text, Title} from "@mantine/core";
 import NavigationButtons from "../NavigationButtons/NavigationButtons.tsx";
 import classes from './ArticleContent.module.css'
 
 const ArticleContent = ({ content }: any) => {
     const briefText = content.pageContent.map((section) => {
         return (
-            <Text>{section.label}</Text>
+            <Text size="lg" pt="xs">{section.label}</Text>
         )
     })
 
     const test = content.pageContent.map((section) => {
         return (
-            <Group pb="xl">{
-                section.paragraphs.map((paragraph) => {
-                    return (
-                        <Text>{paragraph.text}</Text>
-                    )
-                })
-            }</Group>
+            <>
+                <Title>{section.label}</Title>
+                <Group pb="xl">{
+                    section.paragraphs.map((paragraph) => {
+                        return (
+                            <>
+                                <Text>{paragraph.text}</Text>
+                                {
+                                    paragraph.image && <Image
+                                        flex={1}
+                                        radius="md"
+                                        src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+                                    />
+                                }
+                            </>
+                        )
+                    })
+                }</Group>
+            </>
         )
     })
 
@@ -25,9 +37,15 @@ const ArticleContent = ({ content }: any) => {
         <div>
             <Container size={1400}>
                 <Title className={classes.title}>{content.title}</Title>
-                <Text className={classes.description}>{content.description}</Text>
-                {briefText}
-                {test}
+                <Container size={1400} className={classes.briefContainer}>
+                    <Image
+                        className={classes.briefImage}
+                        src={content.briefImage} />
+                    <Box p="xl" className={classes.briefBox}>
+                        <Title className={classes.subtitle}>О чём глава</Title>
+                        {briefText}
+                    </Box>
+                </Container>
                 <NavigationButtons
                     nextPage={content.nav.nextPage}
                     previousPage={content.nav.previousPage}
