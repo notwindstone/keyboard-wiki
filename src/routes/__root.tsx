1923fe35389fc6b0007f49c97c9cf95ece40505f
windstone-aristotle-yellow <notwindstone@gmail.com>
2024-03-17 12:09:21 +0300

    Remove tanstack router devtools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,10 @@
 import {createRootRoute, Outlet, ScrollRestoration} from '@tanstack/react-router'
-import {TanStackRouterDevtools} from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
     component: () => (
         <>
             <ScrollRestoration />
             <Outlet />
-            <TanStackRouterDevtools />
         </>
     ),
 })
